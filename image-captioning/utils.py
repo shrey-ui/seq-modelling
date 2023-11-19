f06e8801de6a85ee29b77d85e4e6afcdf7c62550
smurkute2001@gmail.com <smurkute2001@gmail.com>
2023-11-19 12:23:54 +0530

    added xception model for feat extract

--- image-captioning/utils.py
+++ image-captioning/utils.py
@@ -9,6 +9,15 @@ from transformers import BertTokenizer
 from keras.preprocessing.text import Tokenizer
 from keras.utils import pad_sequences
 from keras.utils import to_categorical
+import pretrainedmodels
+import ssl
+os.environ['KERAS_BACKEND']= "torch"
+from keras_core.applications.xception import Xception
+from keras_core.applications.xception import preprocess_input
+
+ssl._create_default_https_context= ssl._create_unverified_context
+
+
 
 
 def get_caption(path_of_caption_file):
@@ -18,19 +27,27 @@ def get_caption(path_of_caption_file):
         dict_captions[row['image']].append(row['caption'])
     return dict_captions, captions
 
+
 def file2img(img_path):
     img= Image.open(f'./data/Flickr8k/Images/{img_path}')
+    img= img.resize((299, 299))
+    #img= np.expand_dims(img, axis= 0)
+    #img= img/127.5
     trans= transforms.Compose([transforms.ToTensor()])
     img_tensor= trans(img)
     return img_tensor
+
+
+
 class ImageCaptionDataset(Dataset):
-    def __init__(self, caption_file_path, tokenizer, max_length, vocab_size):
+    def __init__(self, caption_file_path, tokenizer, max_length, vocab_size, feat_ext_model):
         self.file_names= os.listdir('./data/Flickr8k/Images/')
         self.caption_file_path= caption_file_path
         self.captions_dict, self.caption_df= get_caption(caption_file_path)
         self.tokenizer= tokenizer
         self.max_length= max_length
         self.vocab_size= vocab_size
+        self.cnn= feat_ext_model
 
     def __len__(self):
         return len(self.captions_dict)
@@ -54,6 +71,13 @@ class ImageCaptionDataset(Dataset):
         image_tens= file2img(img)
         inp1 = []
         out= []
+        input_img= image_tens.unsqueeze(0)
+        input_img= torch.permute(input_img, (0, 2, 3, 1))
+        print(input_img.shape)
+        input_img= torch.autograd.Variable(input_img)
+        
+        features= self.cnn.predict(input_img)
+
         for capt_examp in captions:
             print(capt_examp)
             tokens= self.tokenizer.texts_to_sequences([capt_examp])[0]
@@ -61,7 +85,8 @@ class ImageCaptionDataset(Dataset):
             inp1.append(inp_seq)
             out.append(out_seq)
         
-        return [image_tens, inp1], out
+        print(features.shape)
+        return [features, inp1], out
 
 def create_tokenizer(caption_corpus):
     tokenizer= Tokenizer()
@@ -77,10 +102,15 @@ if __name__ == '__main__':
     
     vocab_size= len(tokenizer.word_index) + 1
     max_len= max(len(d.split()) for d in capt_list)
+    
+    model= Xception(include_top= False, pooling= 'avg')
+    
+
 
     CaptionDataset= ImageCaptionDataset('./data/Flickr8k/captions.txt', 
                                         tokenizer, max_len,
-                                        vocab_size)
+                                        vocab_size, 
+                                        model)
 
     train_dl= DataLoader(CaptionDataset, batch_size= 1, shuffle= True)
     print(list(train_dl)[0]) 
